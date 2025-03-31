21127f1cd0e09e0b68398af6c36db4b48e0f0dfc
“micro” <z17582851840@163.com>
2025-03-31 22:41:08 +0800

    请勾选同一git仓库下的文件后重试

---
+++ mall-order/src/main/java/cn/bugstack/mall/order/config/SeataConfigure.java
@@ -0,0 +1,34 @@
+package cn.bugstack.mall.order.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import io.seata.rm.datasource.DataSourceProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+
+/**
+ * @author micro, 微信：yykk、
+ * @description
+ * @date 2025/3/31 22:25
+ * @github https://github.com/tokyokk
+ * @copyright 博客：http://bugstack.cc - 沉淀、分享、成长。让自己和他人都有所收获！
+ */
+@Configuration
+public class SeataConfigure {
+
+    @Autowired
+    private DataSourceProperties dataSourceProperties;
+
+    @Bean
+    public DataSource dataSource(DataSourceProperties dataSourceProperties) {
+        HikariDataSource dataSource = dataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+        if (StringUtils.hasText(dataSourceProperties.getName())) {
+            dataSource.setPoolName(dataSourceProperties.getName());
+        }
+        return new DataSourceProxy(dataSource);
+    }
+}
