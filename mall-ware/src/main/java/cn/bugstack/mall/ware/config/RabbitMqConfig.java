777c7354a147b828325d1a76b8fa850ff3d5aaf8
“micro” <z17582851840@163.com>
2025-04-06 20:17:47 +0800

    移除Seata依赖，ware集成RabbitMQ

---
+++ mall-ware/src/main/java/cn/bugstack/mall/ware/config/RabbitMqConfig.java
@@ -0,0 +1,70 @@
+package cn.bugstack.mall.ware.config;
+
+import com.google.common.collect.Maps;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+/**
+ * @author micro, 微信：yykk、
+ * @description
+ * @date 2025/4/6 19:58
+ * @github https://github.com/tokyokk
+ * @copyright 博客：http://bugstack.cc - 沉淀、分享、成长。让自己和他人都有所收获！
+ */
+@Configuration
+public class RabbitMqConfig {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Exchange stockEventExchange() {
+        return new TopicExchange("stock-event-exchange", true, false);
+    }
+
+    @Bean
+    public Queue stockReleaseStockQueue() {
+        return new Queue("stock.release.stock.queue", true, false, false);
+    }
+
+    @Bean
+    public Queue stockDelayQueue() {
+         /*
+          延迟队列的参数
+          x-dead-letter-exchange：代表消息过期后转发的交换机
+          x-dead-letter-routing-key：代表消息过期后转发的路由键
+          x-message-ttl：代表消息的过期时间，单位毫秒
+         */
+        final Map<String, Object> arguments = Maps.newHashMap();
+        arguments.put("x-dead-letter-exchange", "stock-event-exchange");
+        arguments.put("x-dead-letter-routing-key", "stock.release");
+        arguments.put("x-message-ttl", 120000);
+
+        return new Queue("stock.delay.queue", true, false, false, arguments);
+    }
+
+    @Bean
+    public Binding stockReleaseBinding() {
+        return new Binding("stock.release.stock.queue", Binding.DestinationType.QUEUE, "stock-event-exchange", "stock.release.#", null);
+    }
+
+    @Bean
+    public Binding stockLockedBinding() {
+        return new Binding("stock.delay.queue", Binding.DestinationType.QUEUE, "stock-event-exchange", "stock.locked", null);
+    }
+}
