88c885d9e483343722fd70f5c1effa6aab42a006
“micro” <z17582851840@163.com>
2025-05-08 00:12:44 +0800

    feat：集成sleuth+zipkin实现链路追踪

---
+++ mall-gateway/src/main/java/cn/bugstack/mall/gateway/config/SentinelGatewayConfig.java
@@ -0,0 +1,34 @@
+package cn.bugstack.mall.gateway.config;
+
+import cn.bugstack.common.exception.BizCodeEnum;
+import cn.bugstack.common.utils.R;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import com.alibaba.fastjson.JSON;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author micro, 微信：yykk、
+ * @description
+ * @date 2025/5/7 21:12
+ * @github https://github.com/tokyokk
+ * @copyright 博客：http://bugstack.cc - 沉淀、分享、成长。让自己和他人都有所收获！
+ */
+@Configuration
+public class SentinelGatewayConfig {
+
+    public SentinelGatewayConfig() {
+        GatewayCallbackManager.setBlockHandler(new BlockRequestHandler() {
+            // 网关限流了请求，就会调用这个回调
+            @Override
+            public Mono<ServerResponse> handleRequest(ServerWebExchange serverWebExchange, Throwable throwable) {
+                R error = R.error(BizCodeEnum.TOO_MANY_REQUEST.getCode(), BizCodeEnum.TOO_MANY_REQUEST.getMsg());
+                String errorJson = JSON.toJSONString(error);
+                return ServerResponse.ok().body(Mono.just(errorJson), String.class);
+            }
+        });
+    }
+}
